a95f78e5fcc0ff2ff8910b17b458703627d2a0fb
Olivera Radovanović <90214731+Olivera2708@users.noreply.github.com>
2024-04-27 23:37:23 +0200

    second

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 from llamaapi import LlamaAPI
 from openai import OpenAI
+import regex as re
 
 # Initialize the SDK
 llama = LlamaAPI("LL-Z1MNZGxiDuLn5B0jNrIpf9NTQo2kEJf5V9mVqE6Nkdqi4dY6aE6ZMN2MLVMV7gjk")
@@ -11,12 +12,12 @@ def get_spending_tip():
     response = client.chat.completions.create(
         model="llama-13b-chat",
         messages=[
-            {"role": "system", "content": "Assistant is a large language model trained by OpenAI."},
-            {"role": "user", "content": "Give me an original financial tip for monthly spending up to 10 words"}
+            {"role": "system", "content": "You are a proffesional financial advisor. Your answers should be serious and proffesional."},
+            {"role": "user", "content": "Give me a very short original financial tip for monthly spending up to 10 words"}
         ],
-        temperature=2
+        temperature=0.8
     )
-    return response.choices[0].message.content
+    return remove_emojis(response.choices[0].message.content.split(".")[0].replace("\"", ""))
 
 def get_category_tip():
     with open("data.json") as file:
@@ -45,6 +46,22 @@ def get_category(word):
     return response.choices[0].message.content
 
 
+def remove_emojis(text):
+    emoji_pattern = re.compile(
+        "[\U0001F600-\U0001F64F]|"  # emoticons
+        "[\U0001F300-\U0001F5FF]|"  # symbols & pictographs
+        "[\U0001F680-\U0001F6FF]|"  # transport & map symbols
+        "[\U0001F700-\U0001F77F]|"  # alchemical symbols
+        "[\U0001F780-\U0001F7FF]|"  # Geometric Shapes Extended
+        "[\U0001F800-\U0001F8FF]|"  # Supplemental Arrows-C
+        "[\U0001F900-\U0001F9FF]|"  # Supplemental Symbols and Pictographs
+        "[\U0001FA00-\U0001FA6F]|"  # Chess Symbols
+        "[\U0001FA70-\U0001FAFF]",  # Symbols and Pictographs Extended-A
+        flags=re.UNICODE
+    )
+    cleaned_text = emoji_pattern.sub('', text)
+    return cleaned_text
+
 
 if __name__=="__main__":
-    print(get_category("Coffee with friends"))
+    print("Spending tip -> " + get_spending_tip())
